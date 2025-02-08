7dfdc260ad3ed2b65c0864eb328c84ac018dd345
Surendar Yama <surendaryama@outlook.com>
2025-02-08 18:25:51 +0530

    Dependency Injection (DI) init.

--- index.js
+++ index.js
@@ -2,17 +2,29 @@ console.log('Happy developing ✨');
 (function () {
     "use strict";
     angular.module("myFirstApp", [])
-        .controller("MyFirstController", function ($scope) {
+        .controller("MyFirstController", MyFirstController)
+        .controller("NameCalculatorAppController", NameCalculatorAppController )
+        .controller("DIController", DIController)
+
+        function MyFirstController ($scope) {
             $scope.message = "Hello World!";
             $scope.sayHi = () => "Hi.";
-        })
-        .controller("NameCalculatorAppController", function ($scope) {
+        }
+        function NameCalculatorAppController ($scope) {
             $scope.name = "";
             $scope.totalValue = 0;
             $scope.displayNumeric = () => {
                 $scope.totalValue = calculateNumericForString($scope.name);
             }
-        })
+        }
+
+        function DIController ($scope, $filter) {
+            $scope.name = "Surendar";
+            $scope.upper = function () {
+                let upCase = $filter("uppercase");
+                $scope.name = upCase($scope.name);
+            }
+        };
 
 
         function calculateNumericForString(string){
